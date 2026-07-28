5ac64622b074a615cfeaaae2c45d9188734ef779
Gian-Reto Tarnutzer <gian.reto.tarnutzer@threema.ch>
2026-07-28 19:20:13 +0200

    Fix missing Vitest headless flag

--- packages/dom/vitest.config.ts
+++ packages/dom/vitest.config.ts
@@ -14,6 +14,7 @@ export default defineConfig({
         include: ['src/**/*.test.ts'],
         browser: {
             enabled: true,
+            headless: true,
             instances: [{browser: 'chromium'}],
             provider: playwright({
                 contextOptions: {
